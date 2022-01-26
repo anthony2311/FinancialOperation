2c2738e2d29afbca526447c936cfee4b16a5f6f9
Anthony Lammens <anthony.lammens@outlook.com>
2022-01-26 19:26:24 +0100

    Creating Report method to manage multiple financial operations

---
+++ FinancialOperation/FinancialOperation.lib/FinancialOperationImporter.cs
@@ -0,0 +1,46 @@
+﻿using FinancialOperation.lib.Models;
+using System;
+using System.Globalization;
+
+namespace FinancialOperation.lib
+{
+    public static class FinancialOperationImporter
+    {
+        /// <summary>
+        /// Transform multiple operation as string to a report
+        /// </summary>
+        /// <param name="operations">string as "[Operation][amount]\n". Possible operations are P, R, F and L. Amount is optional with L</param>
+        /// <returns>string containing Balance, Total fees and Transferred to recipient</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static string Report(string operations)
+        {
+            var operationList = operations.Split("\n");
+            Account account = new Account();
+            foreach (var operation in operationList)
+            {
+                if (string.IsNullOrEmpty(operation))
+                    continue;
+
+                switch (operation[0])
+                {
+                    case 'P':
+                        account.Payment(decimal.Parse(operation.Substring(1), CultureInfo.InvariantCulture));
+                        break;
+                    case 'R':
+                        account.Refund(decimal.Parse(operation.Substring(1), CultureInfo.InvariantCulture));
+                        break;
+                    case 'F':
+                        account.TransactionFee(decimal.Parse(operation.Substring(1), CultureInfo.InvariantCulture));
+                        break;
+                    case 'L':
+                        account.Payout();
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unknown operation {operation[0]}");
+                }
+            }
+
+            return account.ToString();
+        }
+    }
+}
---
+++ FinancialOperation/FinancialOperation.lib/Models/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FinancialOperation.lib.Models
+{
+    internal class Account
+    {
+        private decimal _balance = 0;
+        private decimal _fees = 0;
+        private decimal _transferredToRecipient = 0;
+        public Account()
+        {
+        }
+
+        public void Payment(decimal amount)
+        {
+            // TODO : check wanted behavior on negative amount
+            _balance += amount;
+        }
+        public void Refund(decimal amount)
+        {
+            // TODO : check wanted behavior on negative amount
+            _balance -= amount;
+        }
+        public void TransactionFee(decimal amount)
+        {
+            // TODO : check wanted behavior on negative amount
+            _balance -= amount;
+            _fees += amount;
+        }
+        public void Payout()
+        {
+            _transferredToRecipient = _balance;
+            _balance = 0;
+        }
+
+        public override string ToString() {
+            return $"Balance: {_balance} euros{Environment.NewLine}" +
+                $"Total fees: {_fees} euros{Environment.NewLine}" +
+                $"Transferred to recipient: {_transferredToRecipient} euros";
+        }
+    }
+}
